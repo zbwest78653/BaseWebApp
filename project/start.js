29dd778659cb468c20cbf6a80f5aff5879e7fb6f
Zach West <zacharybwest@gmail.com>
2023-01-10 13:43:17 -0600

    api

--- project/start.js
+++ project/start.js
@@ -13,6 +13,15 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
+app.get('/alerts', function(request, response) {
+  response.render('pages/alerts');
+});
+
+app.get('/data-sergeant', function(request, response) {
+  response.render('pages/data-sergeant');
+});
+
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
